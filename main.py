8a8fcc694a259bb628b2df032451de83cf4fe4f3
Armando Zubillaga <ajzpiv97@gmail.com>
2020-09-19 15:19:30 -0400

    Added print statement

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def _get_jwt(user_data):
     payload = {'exp': exp_time,
                'nbf': datetime.datetime.utcnow(),
                'email': user_data['email']}
-    print(payload)
+    print(payload, 'It works')
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
 
 if __name__ == '__main__':
